8c1c9722769d180f39f856e7a7110f50a4c6f230
LesterCuasay <lestercuasay@live.co.uk>
2023-10-16 14:24:44 +0000

    feat - added test cases for logged in and logged out users creating tasks

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,67 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Task
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class TestListViewTests(APITestCase):
+
+    def setUp(self):
+        '''
+        Sets up a user to be accessed in the Test Cases
+        '''
+
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpassword',
+        )
+
+    def test_logged_in_user_can_view_list_task(self):
+        '''
+        Tests if a logged in user to check if they can see the task list view
+        '''
+
+        self.client.login(
+            username='testuser',
+            password='testpassword',
+        )
+
+        Task.objects.create(owner=self.user, task_name='A task')
+
+        response = self.client.get('/tasks/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_logged_out_user_can_view_list_task(self):
+        '''
+        Tests if a logged out user can check the task list view
+        '''
+
+        Task.objects.create(owner=self.user, task_name='A task')
+
+        response = self.client.get('/tasks/')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_logged_in_user_can_create_task(self):
+        '''
+        Tests if a logged in user can create a task
+        '''
+
+        self.client.login(
+            username='testuser',
+            password='testpassword',
+        )
+
+        response = self.client.post('/tasks/', {'task_name': 'A task'})
+        count = Task.objects.count()
+        self.assertEqual(count, 1)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_logged_out_user_can_create_task(self):
+        '''
+        Tests if a logged out user can create a task
+        '''
+
+        response = self.client.post('/tasks/', {'task_name': 'A task'})
+        count = Task.objects.count()
+        self.assertEqual(count, 0)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
