c30e191dca104482ea386691623052bf5d05c90e
LesterCuasay <lestercuasay@live.co.uk>
2023-10-16 18:41:16 +0000

    feat - added a serializer for NotesDetail

--- notes/serializers.py
+++ notes/serializers.py
@@ -5,6 +5,8 @@ from notes.models import Notes
 class NotesSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -13,6 +15,11 @@ class NotesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Notes
         fields = [
-            'owner', 'is_owner', 'task', 'created_at', 'updated_at',
-            'task_requirements',
+            'owner', 'is_owner', 'profile_id', 'profile_image', 'task',
+            'created_at', 'updated_at', 'task_requirements',
         ]
+
+
+class NotesDetailSerializer(serializers.ModelSerializer):
+
+    task = serializers.ReadOnlyField(source='task.id')
