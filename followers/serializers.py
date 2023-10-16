a32afc023ed1fd1e402c8e72e6f9e87da941e910
LesterCuasay <lestercuasay@live.co.uk>
2023-10-16 19:44:12 +0000

    feat - created follower serializer

---
+++ followers/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Follower
+
+
+class FollowerSerializers(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    followed_name = serializers.ReadOnlyField(source='followed.username')
+
+    class Meta:
+        model = Follower
+        fields = [
+            'id', 'owner', 'created_at', 'followed', 'followed_name',
+        ]
