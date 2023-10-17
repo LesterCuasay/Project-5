c61553fe9cf8ee5a72cd68597f2643135d065755
LesterCuasay <lestercuasay@live.co.uk>
2023-10-17 14:08:59 +0000

    feat - added fields to views to check counut of tasks, followers and following

--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,28 @@
-from rest_framework import generics
+from django.db.models import Count
+from rest_framework import generics, filters
 from .models import Profile
 from .serializers import ProfileSerializer
 from taskmaster_api.permissions import IsOwnerOrReadOnly
 
 
 class ProfileList(generics.ListAPIView):
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        tasks_count=Count('owner__task', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True),
+    ).order_by('-created_at')
+
     serializer_class = ProfileSerializer
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'tasks_count',
+        'followers_count',
+        'following_count',
+        'owner__following__created_at',
+        'owner__followed__created_at',
+    ]
 
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
