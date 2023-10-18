eeaf473ebcaea512eb1e645855df1d0cbe3204b0
LesterCuasay <lestercuasay@live.co.uk>
2023-10-18 12:43:06 +0000

    feat - added filterset fields for searchability

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,6 @@
 from django.db.models import Count
 from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Task
 from .serializers import TaskSerializer
 from taskmaster_api.permissions import IsOwner
@@ -7,17 +8,19 @@ from taskmaster_api.permissions import IsOwner
 
 class TaskList(generics.ListCreateAPIView):
     serializer_class = TaskSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get_queryset(self):
-        queryset = Task.objects.annotate(
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Task.objects.annotate(
             notes_count=Count('notes', distinct=True)
-        )
-        return queryset.filter(owner=self.request.user).order_by('-created_at')
+        ).order_by('-created_at')
 
     filter_backends = [
         filters.OrderingFilter,
         filters.SearchFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        'owner__followed__owner__profile',
+        'owner__profile',
     ]
     search_fields = [
         'owner__username',
