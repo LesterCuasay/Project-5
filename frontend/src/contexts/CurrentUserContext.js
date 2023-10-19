b65960d8e4b204e00c40737dc05f60abff0d802a
LesterCuasay <lestercuasay@live.co.uk>
2023-10-19 13:37:57 +0000

    feat - added refresh and acess tokens

--- frontend/src/contexts/CurrentUserContext.js
+++ frontend/src/contexts/CurrentUserContext.js
@@ -1,6 +1,6 @@
 import { createContext, useContext, useEffect, useMemo, useState } from "react";
 import axios from "axios";
-// import { axiosReq, axiosRes } from "../api/axiosDefaults";
+import { axiosReq, axiosRes } from "../api/axiosDefaults";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 export const CurrentUserContext = createContext();
@@ -16,7 +16,7 @@ export const CurrentUserProvider = ({ children }) => {
 
   const handleMount = async () => {
     try {
-      const { data } = await axios.get("dj-rest-auth/user/");
+      const { data } = await axiosRes.get("dj-rest-auth/user/");
       setCurrentUser(data);
     } catch (err) {
       console.log(err);
@@ -27,46 +27,46 @@ export const CurrentUserProvider = ({ children }) => {
     handleMount();
   }, []);
 
-  // useMemo(() => {
-  //   axiosReq.interceptors.request.use(
-  //     async (config) => {
-  //       try {
-  //         await axios.post("/dj-rest-auth/token/refresh/");
-  //       } catch (err) {
-  //         setCurrentUser((prevCurrentUser) => {
-  //           if (prevCurrentUser) {
-  //             history.push("/signin");
-  //           }
-  //           return null;
-  //         });
-  //         return config;
-  //       }
-  //       return config;
-  //     },
-  //     (err) => {
-  //       return Promise.reject(err);
-  //     }
-  //   );
-  //   axiosRes.interceptors.response.use(
-  //     (response) => response,
-  //     async (err) => {
-  //       if (err.response?.status === 401) {
-  //         try {
-  //           await axios.post("/dj-rest-auth/token/refresh/");
-  //         } catch (err) {
-  //           setCurrentUser((prevCurrentUser) => {
-  //             if (prevCurrentUser) {
-  //               history.push("/signin");
-  //             }
-  //             return null;
-  //           });
-  //         }
-  //         return axios(err.config);
-  //       }
-  //       return Promise.reject(err);
-  //     }
-  //   );
-  // }, [history]);
+  useMemo(() => {
+    axiosReq.interceptors.request.use(
+      async (config) => {
+        try {
+          await axios.post("/dj-rest-auth/token/refresh/");
+        } catch (err) {
+          setCurrentUser((prevCurrentUser) => {
+            if (prevCurrentUser) {
+              history.push("/signin");
+            }
+            return null;
+          });
+          return config;
+        }
+        return config;
+      },
+      (err) => {
+        return Promise.reject(err);
+      }
+    );
+    axiosRes.interceptors.response.use(
+      (response) => response,
+      async (err) => {
+        if (err.response?.status === 401) {
+          try {
+            await axios.post("/dj-rest-auth/token/refresh/");
+          } catch (err) {
+            setCurrentUser((prevCurrentUser) => {
+              if (prevCurrentUser) {
+                history.push("/signin");
+              }
+              return null;
+            });
+          }
+          return axios(err.config);
+        }
+        return Promise.reject(err);
+      }
+    );
+  }, [history]);
 
   return (
     <CurrentUserContext.Provider value={currentUser}>
