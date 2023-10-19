5a962ff6706ea8fdc25ce4c0ce97b2b8ae4c2af0
LesterCuasay <lestercuasay@live.co.uk>
2023-10-19 13:12:50 +0000

    feat - created signinform

---
+++ frontend/src/pages/auth/SignInForm.js
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+
+import Alert from "react-bootstrap/Alert";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+
+import styles from "../../styles/SignInUpForm.module.css";
+import btnStyles from "../../styles/Button.module.css";
+import appStyles from "../../App.module.css";
+
+
+import axios from "axios";
+
+const SignInForm = () => {
+
+  const [signInData, setSignInData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const { username, password } = signInData;
+
+  const [errors, setErrors] = useState({});
+
+  const history = useHistory();
+
+  const handleChange = (event) => {
+    setSignInData({
+      ...signInData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const { data } = await axios.post("/dj-rest-auth/login/", signInData);
+      history.push("/");
+    } catch (err) {
+      setErrors(err.response?.data);
+    }
+  };
+
+  return (
+    <Container className={styles.Container}>
+      <Row className="justify-content-center">
+        <Col md={6} className={appStyles.Content}>
+          <div className={styles.TitleWrapper}>
+            <h1 className={styles.FormTitle}>Sign In</h1>
+          </div>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className={styles.FormGroup} controlId="username">
+              <Form.Label className="d-none">Username</Form.Label>
+              <Form.Control
+                className={styles.Input}
+                type="text"
+                placeholder="Username"
+                name="username"
+                value={username}
+                onChange={handleChange}
+              ></Form.Control>
+            </Form.Group>
+
+            {errors.username?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
+
+            <Form.Group className={styles.FormGroup} controlId="password2">
+              <Form.Label className="d-none">Password</Form.Label>
+              <Form.Control
+                className={styles.Input}
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={password}
+                onChange={handleChange}
+              ></Form.Control>
+            </Form.Group>
+
+            <Button
+              className={`${btnStyles.Wide} ${btnStyles.Button}`}
+              type="submit"
+            >
+              Sign In
+            </Button>
+          </Form>
+          <Link className={styles.Link} to="/signup">
+            Already have an account? <span>Sign up here</span>
+          </Link>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default SignInForm;
