916dfd1fb3929fd2f07a1ac048bd7f9d8a879e8d
LesterCuasay <lestercuasay@live.co.uk>
2023-10-19 12:54:49 +0000

    feat - users can now sign up

--- frontend/src/pages/auth/SignUpForm.js
+++ frontend/src/pages/auth/SignUpForm.js
@@ -1,16 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import Alert from "react-bootstrap/Alert";
 
 import appStyles from "../../App.module.css";
-import btnStyles from "../../styles/Button.module.css"
+import btnStyles from "../../styles/Button.module.css";
 import styles from "../../styles/SignInUpForm.module.css";
 
+import axios from "axios";
+
 const SignUpForm = () => {
+  const [signUpData, setSignUpData] = useState({
+    username: "",
+    password1: "",
+    password2: "",
+  });
+  const { username, password1, password2 } = signUpData;
+
+  const [errors, setErrors] = useState({});
+
+  const history = useHistory();
+
+  const handleChange = (event) => {
+    setSignUpData({
+      ...signUpData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post("/dj-rest-auth/registration/", signUpData);
+      history.push("/signin");
+    } catch (err) {
+      setErrors(err.response?.data);
+    }
+  };
   return (
     <Container className={styles.Container}>
       <Row className="justify-content-center">
@@ -18,7 +49,7 @@ const SignUpForm = () => {
           <div className={styles.TitleWrapper}>
             <h1 className={styles.FormTitle}>Sign Up</h1>
           </div>
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Group className={styles.FormGroup} controlId="username">
               <Form.Label className="d-none">Username</Form.Label>
               <Form.Control
@@ -26,8 +57,17 @@ const SignUpForm = () => {
                 type="text"
                 placeholder="Username"
                 name="username"
+                value={username}
+                onChange={handleChange}
               ></Form.Control>
             </Form.Group>
+
+            {errors.username?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
+
             <Form.Group className={styles.FormGroup} controlId="password">
               <Form.Label className="d-none">Password</Form.Label>
               <Form.Control
@@ -35,8 +75,17 @@ const SignUpForm = () => {
                 type="password"
                 placeholder="Password"
                 name="password1"
+                value={password1}
+                onChange={handleChange}
               ></Form.Control>
             </Form.Group>
+
+            {errors.password1?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
+
             <Form.Group className={styles.FormGroup} controlId="password2">
               <Form.Label className="d-none">Password</Form.Label>
               <Form.Control
@@ -44,9 +93,19 @@ const SignUpForm = () => {
                 type="password"
                 placeholder="Confirm Password"
                 name="password2"
+                value={password2}
+                onChange={handleChange}
               ></Form.Control>
             </Form.Group>
-            <Button className={`${btnStyles.Wide} ${btnStyles.Button}`}>Sign Up</Button>
+
+            {errors.password2?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
+            <Button className={`${btnStyles.Wide} ${btnStyles.Button}`} type="submit">
+              Sign Up
+            </Button>
           </Form>
         </Col>
       </Row>
