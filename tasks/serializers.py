8a5e39e6a04893ead2df8d8ef32889e9dc24fd4d
LesterCuasay <lestercuasay@live.co.uk>
2023-10-23 09:42:14 +0000

    feat - added favourite_id in task serializer

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Task
+from favourites.models import Favourite
 
 
 class TaskSerializer(serializers.ModelSerializer):
@@ -7,11 +8,21 @@ class TaskSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    favourite_id = serializers.SerializerMethodField()
     notes_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+
+    def get_favourite_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            favourite = Favourite.objects.filter(
+                owner=user, task=obj
+            ).first()
+            return favourite.id if favourite else None
+        return None
 
     def validate_attachment(self, value):
         if value:
@@ -27,5 +38,5 @@ class TaskSerializer(serializers.ModelSerializer):
             'id', 'owner', 'task_name', 'task_description',
             'due_date', 'created_at', 'updated_at', 'status', 'attachment',
             'profile_id', 'profile_image', 'is_owner', 'is_overdue',
-            'notes_count',
+            'favourite_id', 'notes_count',
         ]
