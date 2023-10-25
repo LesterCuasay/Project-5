db8845e9fa1da3d170c8064b08f50833f640ab82
LesterCuasay <lestercuasay@live.co.uk>
2023-10-25 15:08:54 +0000

    feat - refactored code and created ProfileDataCOntext

--- frontend/src/pages/profiles/PopularProfiles.js
+++ frontend/src/pages/profiles/PopularProfiles.js
@@ -1,38 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 import Container from "react-bootstrap/Container";
 import appStyles from "../../App.module.css";
-import { axiosReq } from "../../api/axiosDefaults";
-import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import Asset from "../../components/Asset";
 import Profile from "./Profile";
+import { useProfileData } from "../../contexts/ProfileDataContext";
 
 const PopularProfiles = ({ mobile }) => {
-  const [profileData, setProfileData] = useState({
-    pageProfile: { results: [] },
-    popularProfiles: { results: [] },
-  });
-
-  const { popularProfiles } = profileData;
-  const currentUser = useCurrentUser();
-
-  useEffect(() => {
-    const handleMount = async () => {
-      try {
-        const { data } = await axiosReq.get(
-          "/profiles/?ordering=-followers_count"
-        );
-        setProfileData((prevState) => ({
-          ...prevState,
-          popularProfiles: data,
-        }));
-      } catch (err) {
-        console.log(err);
-      }
-    };
-
-    handleMount();
-  }, [currentUser]);
+  const { popularProfiles } = useProfileData();
 
   return (
     <Container
@@ -46,12 +21,12 @@ const PopularProfiles = ({ mobile }) => {
           {mobile ? (
             <div className="d-flex justify-content-around">
               {popularProfiles.results.slice(0, 4).map((profile) => (
-                <Profile key={profile.id} profile={profile} mobile/>
+                <Profile key={profile.id} profile={profile} mobile />
               ))}
             </div>
           ) : (
             popularProfiles.results.map((profile) => (
-              <Profile key={profile.id} profile={profile}/>
+              <Profile key={profile.id} profile={profile} />
             ))
           )}
         </>
