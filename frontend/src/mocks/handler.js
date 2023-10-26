89e311379f76d6a4d97f2d30d46bcbafd17dcba9
LesterCuasay <lestercuasay@live.co.uk>
2023-10-26 15:11:32 +0000

    feat - setup frontend for testing

---
+++ frontend/src/mocks/handler.js
@@ -0,0 +1,21 @@
+const baseURL = "http://localhost:8000/";
+
+export const handlers = [
+  rest.get(`${baseURL}dj-rest-auth/user/`, (req, res, ctx) => {
+    return res(
+      ctx({
+        pk: 4,
+        username: "rob",
+        email: "",
+        first_name: "",
+        last_name: "",
+        profile_id: 4,
+        profile_image:
+          "https://res.cloudinary.com/dy6ugjuru/image/upload/v1/media/../default_profile_pmqhl9",
+      })
+    );
+  }),
+  rest.post(`${baseURL}dj-rest-auth/logout/`, (req, res, ctx) => {
+    return res(ctx.status(200))
+  })
+];
