990a1cc29ea361e706180648e8938b963f2436f2
LesterCuasay <lestercuasay@live.co.uk>
2023-11-14 20:41:29 +0000

    feat - changed assertion equal to 200

--- tasks/tests.py
+++ tasks/tests.py
@@ -42,7 +42,7 @@ class TestListViewTests(APITestCase):
         Task.objects.create(owner=self.user, task_name='A task')
 
         response = self.client.get('/tasks/')
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_logged_in_user_can_create_task(self):
         '''
